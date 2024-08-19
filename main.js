601313b3516a1eadf3795b01d3b621c74c2704d8
PedroOtaviano <pedro.redes23@gmail.com>
2024-08-19 18:46:20 -0400

    Aplicando estilos e transformand inputs em numeros.

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 const form = document.getElementById("form");
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
 
-    console.log("Formulário enviado");
-
-    const NumeroA = document.querySelector(".numberA").value;
-    const NumeroB = document.querySelector(".numberB").value;
-
-
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
     
-    if (NumeroA > NumeroB) {
-        document.querySelector(".error").style.display = "block";
-    } else if (NumeroA == NumeroB) {
-        document.querySelector(".equals").style.display = "block";
-    } else {
-        document.querySelector(".success").style.display = "block";
-    }
+        console.log("Formulário enviado");
+    
+        // Converte os valores para números
+        const NumeroA = parseInt(document.querySelector(".numberA").value, 10);
+        const NumeroB = parseInt(document.querySelector(".numberB").value, 10);
+    
+        const mensagem = `Número A: ${NumeroA} e Número B: ${NumeroB}`;
+        
+        
+        if (NumeroA > NumeroB) {
+            document.querySelector(".error").style.display = "block";
+        } else if (NumeroA === NumeroB) {
+            document.querySelector(".equals").style.display = "block";
+        } else {
+            document.querySelector(".success").style.display = "block";
+        }
+    });
 
-});
+
 
 form.addEventListener("click", function(e){
     document.querySelector(".error").style.display = "none";
